b56113a44f8de71039d913838d8e1802eaf1fa55
Cong Ngo <you@example.com>
2023-08-28 09:40:47 +0700

    add put api

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from typing import List
 from uuid import UUID
 from fastapi import FastAPI, HTTPException
 
-from models import User, Gender, Role
+from models import User, Gender, Role, UserUpdateRequest
 
 app = FastAPI()
 
@@ -54,3 +54,20 @@ async def delete_user(user_id: UUID):
     )
 
 # Put: update an object 
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+        status_code = 404,
+        detail= f"User with id: {user_id} does not exists."
+    )
